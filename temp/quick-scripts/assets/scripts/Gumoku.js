ac0ebcd586af554e0121780911698e6ac025cb66
jason zhang <zhj.pavel@gmail.com>
2019-03-29 17:51:50 +0800

    now can play the game with myself.

--- temp/quick-scripts/assets/scripts/Gumoku.js
+++ temp/quick-scripts/assets/scripts/Gumoku.js
@@ -39,12 +39,21 @@ cc.Class({
             default: null,
             type: cc.Node,
             visible: false
+        },
+        maskLayoutNode: {
+            default: null,
+            type: cc.Node
         }
     },
+    //初始化棋盘
     initiateChessBoard: function initiateChessBoard() {
+        //
+        this.maskLayoutNode.active = false;
         var $this = this;
-        for (var x = 0; x < 15; x++) {
-            for (var y = 0; y < 15; y++) {
+        for (var y = 0; y < 15; y++) {
+            //纵坐标
+            for (var x = 0; x < 15; x++) {
+                //横坐标
                 var newChess = cc.instantiate(this.chessPrefab);
                 this.node.addChild(newChess);
                 newChess.setPosition(cc.v2(x * 40 + 20, y * 40 + 20));
@@ -71,74 +80,80 @@ cc.Class({
     //判定胜负
     judgeOver: function judgeOver() {
         console.log("开始判定胜负");
+        var currentPlayer = this.gameState == 'white' ? 'black' : 'white';
         var tag = this.lastChess.getComponent('chess').tag;
         var x = tag % 15;
         var y = (tag - x) / 15;
+        console.log("X = " + x + ";Y = " + y);
         var lastSpriteFrame = this.lastChess.getComponent(cc.Sprite).spriteFrame;
         //横向
+
         var horizental = 0;
-        var isLinked = true; //是否是连子
+
         for (var i = x - 4; i <= x + 4; i++) {
             //前四后四一共9颗子
             if (i >= 0 && i < 15) {
-                var frame = this.chessList[i * 15 + y].getComponent(cc.Sprite).spriteFrame;
-                if (lastSpriteFrame == frame && isLinked) {
+                // this.chessList[y*15+i].getComponent(cc.Sprite).spriteFrame = this.whiteChessFrame;
+
+                var frame = this.chessList[y * 15 + i].getComponent(cc.Sprite).spriteFrame;
+                console.log(i + " horizental.equals = " + (frame == lastSpriteFrame), "lastSprite", lastSpriteFrame, "current", frame);
+                if (lastSpriteFrame == frame) {
                     horizental++; //连子数量
-                    isLinked = true;
-                } else {
-                    //horizental = 0;//重置连子数量
-                    if (horizental > 0) {
-                        isLinked = false;
+                    console.log(currentPlayer + '横向连子数量:' + horizental);
+                    if (horizental >= 5) {
+                        this.showWinLayout(currentPlayer);
+                        return;
                     }
+                } else {
+                    horizental = 0; //重置连子数量
                 }
             }
         }
-        console.log(this.gameState + '横向连子数量:' + horizental);
-        if (horizental >= 5) {
-            console.log(this.gameState + " win !");
-            return;
-        }
+
         //纵向
         var vertical = 0;
-        isLinked = true;
         for (var i = y - 4; i <= y + 4; i++) {
             if (i >= 0 && i < 15) {
-                var frame = this.chessList[y * 15 + i].getComponent(cc.Sprite).spriteFrame;
-                if (lastSpriteFrame == frame && isLinked) {
+                var index = i * 15 + x;
+                // this.chessList[index].getComponent(cc.Sprite).spriteFrame = this.whiteChessFrame;
+
+                var frame = this.chessList[index].getComponent(cc.Sprite).spriteFrame;
+                if (lastSpriteFrame == frame) {
                     vertical++; //连子数量
-                    isLinked = true;
-                } else {
-                    //horizental = 0;//重置连子数量
-                    if (vertical > 0) {
-                        isLinked = false;
+                    console.log(currentPlayer + "纵向连子数量:" + vertical);
+                    if (vertical >= 5) {
+                        this.showWinLayout(currentPlayer);
+                        return;
                     }
+                } else {
+                    vertical = 0; //重置连子数量
                 }
             }
         }
-        console.log(this.gameState + "纵向连子数量:" + vertical);
-        if (vertical >= 5) {
-            console.log(this.gameState + " win !");
-            return;
-        }
+
         //左斜（第一与第三象限）
+
         var leftSlash = 0;
-        var f = y - x; //斜率K = (y2-y1)/(x2-x1); 此k=1，y2=x2+y1-x1; 设f = y1-x1,那么y2 = x2+f，z = y2*15+x2;遍历x2,z = (f+x2)*15+x2
+        var f = y - x; //斜率K = (y1-y)/(x1-x); 此k=1，y1=x1+y-x; 设f = y-x,那么y1 = x1+f，z = y*15+x;遍历x,z = (f+x)*15+x
         for (var i = 0; i < 15; i++) {
             if (f + i < 0 || f + i > 14) {
                 continue;
             }
+            // this.chessList[(f+i)*15+i].getComponent(cc.Sprite).spriteFrame = this.whiteChessFrame;
+
             var frame = this.chessList[(f + i) * 15 + i].getComponent(cc.Sprite).spriteFrame;
             if (frame == lastSpriteFrame) {
                 leftSlash++;
+                console.log(currentPlayer + "左斜连子数量:" + leftSlash);
+                if (leftSlash >= 5) {
+                    this.showWinLayout(currentPlayer);
+                    return;
+                }
             } else {
                 leftSlash = 0;
             }
         }
-        console.log(this.gameState + "左斜连子数量:" + leftSlash);
-        if (leftSlash >= 5) {
-            console.log(this.gameState + " win !");
-            return;
-        }
+
         //右斜
         var rightSlash = 0;
         f = y + x;
@@ -146,17 +161,32 @@ cc.Class({
             if (f - i < 0 || f - i > 14) {
                 continue;
             }
+            // this.chessList[(f-i)*15+i].getComponent(cc.Sprite).spriteFrame = this.whiteChessFrame;
+
             var frame = this.chessList[(f - i) * 15 + i].getComponent(cc.Sprite).spriteFrame;
             if (frame == lastSpriteFrame) {
                 rightSlash++;
+                console.log(currentPlayer + "右斜连子数量:" + rightSlash);
+                if (rightSlash >= 5) {
+                    this.showWinLayout(currentPlayer);
+                    return;
+                }
             } else {
                 rightSlash = 0;
             }
         }
-        console.log(this.gameState + "右斜连子数量:" + rightSlash);
-        if (rightSlash >= 5) {
-            console.log(this.gameState + " win !");
-            return;
+    },
+
+    //展示胜负结果（player = 'black' or 'white'）
+    showWinLayout: function showWinLayout(player) {
+        this.maskLayoutNode.getChildByName("judgeResult").getComponent(cc.Label).string = player + " win !";
+        this.maskLayoutNode.active = true;
+        console.log(player + " win !");
+    },
+    replayGomuku: function replayGomuku() {
+        this.maskLayoutNode.active = false;
+        for (var i in this.chessList) {
+            this.chessList[i].getComponent(cc.Sprite).spriteFrame = null;
         }
     },
 
